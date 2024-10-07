93f6012783f069f5d0333f4da987d143b7546f4c
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-10-07 16:32:38 -0400

    started "form"

--- index.php
+++ index.php
@@ -14,8 +14,18 @@ Scriviamo tutto (logica e layout) in un unico file index.php
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Password</title>
+      <!-- Bootstrap -->
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-    <h1>ciao</h1>
+    <main>
+        <h1>Strong Password Generator</h1>
+        <h2>Genera una password sicura</h2>
+        <section>
+            <form action="index.php" method="get">
+                    
+            </form>
+        </section>
+    </main>
 </body>
 </html>
