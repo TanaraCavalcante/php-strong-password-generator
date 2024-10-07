9d5343bc0920ba5ff108c53494cf5bcaa588f7b7
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-10-07 16:26:15 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!-- Milestone 1
+Creare un form che invii in GET la lunghezza della password.
+Una nostra funzione utilizzerà questo dato per generare una password casuale (composta da lettere, lettere maiuscole, numeri e simboli) da restituire all’utente.
+Scriviamo tutto (logica e layout) in un unico file index.php
+-->
+
+<?php 
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Password</title>
+</head>
+<body>
+    <h1>ciao</h1>
+</body>
+</html>
