b123b5296a6f88a834e541786aaab6ec3cab8a60
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-10-07 16:57:46 -0400

    milestone 1 ok

--- index.php
+++ index.php
@@ -5,7 +5,11 @@ Scriviamo tutto (logica e layout) in un unico file index.php
 -->
 
 <?php 
+$userLength = $_GET["legth"];
 
+function getStrongPassword(){
+
+}
 ?>
 
 <!DOCTYPE html>
@@ -18,12 +22,40 @@ Scriviamo tutto (logica e layout) in un unico file index.php
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-    <main>
+    <main class="container">
         <h1>Strong Password Generator</h1>
         <h2>Genera una password sicura</h2>
         <section>
             <form action="index.php" method="get">
-                    
+            <div class="mb-3">
+                <label for="length" class="form-label">Lunghezza della password:</label>
+                <input type="number" name="length" class="form-control" style="width: 15rem;" id="legth">
+            </div>
+            <div >
+                <div class="form-label">Consenti ripetizioni di uno o più caratteri:</div>
+                <div class="mb-1">
+                    <input type="radio" name="yes" id="yes">
+                    <label for="yes" class="form-label">Si</label>
+                </div>
+                <div class="mb-3">
+                    <input type="radio" name="yes" id="No">
+                    <label for="No" class="form-label">No</label>
+                </div>
+                <div class="mb-1">
+                    <input type="checkbox" name="letter" id="letter">
+                    <label for="letter" class="form-label">Lettera</label>
+                </div>
+                <div class="mb-1">
+                    <input type="checkbox" name="number" id="number">
+                    <label for="number" class="form-label">Number</label>
+                </div>
+                <div class="mb-1">
+                    <input type="checkbox" name="simbol" id="simbol">
+                    <label for="simbol" class="form-label">Simbolo</label>
+                </div>
+                
+            </div>
+
             </form>
         </section>
     </main>
